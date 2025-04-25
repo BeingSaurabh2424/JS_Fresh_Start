12a48b990e9b9d09c3219a3537b07af092f72686
BeingSaurabh2424 <saurabhwadekar24@gmail.com>
2025-04-25 18:33:54 +0530

    code

---
+++ 4. primitve_objects/index.js
@@ -0,0 +1,21 @@
+// nn bb ss u - Primitives in JS
+let a = null;
+let b = 345;
+let c = true;
+let d = BigInt("666") + BigInt("668")
+let e = "SanjuBaba"
+let f = Symbol("a,b,c,d,e,f")
+let g
+console.log(a,b,c,d,e,f,g)
+
+//Non-Primitives i.e objects in JS
+
+const item = {
+    "Saurabh":true,
+    "Shubham":false,
+    "Aditya":true,
+    "Rohan":undefined
+}
+
+console.log(item["Aditya"])
+console.log(item["Rohan"])
