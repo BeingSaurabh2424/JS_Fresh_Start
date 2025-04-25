5def9412a8026cc90bf679c866191b72f69ccdbb
BeingSaurabh2424 <saurabhwadekar24@gmail.com>
2025-04-25 19:03:22 +0530

    js

---
+++ 6. Operators/index.js
@@ -0,0 +1,49 @@
+console.log("Operators in JS")
+
+let a = 10;
+let b = 4;
+//Arithmetic Operators
+console.log("Addition",a+b);
+console.log("Substraction",a-b);
+console.log("Multiplication",a*b);
+console.log("Division",a/b);
+
+console.log("a**b",a**b);
+console.log("a%b",a%b);
+console.log("++a",++a);
+console.log("a++",a++)
+console.log("--a",a--);
+console.log("a--", a--);
+console.log("a = " ,a);
+
+//Assignment Operators
+
+let assignment = 1;
+
+assignment += 5
+console.log("a is now " , assignment)
+
+assignment -=5
+console.log("a is now " , assignment)
+
+assignment *=5
+console.log("a is now " , assignment)
+
+assignment /=5
+console.log("a is now " , assignment)
+
+//comparison operators
+ let comp1 = 6;
+ let comp2 = 7;
+
+ console.log(com1==comp2);
+ console.log
+
+
+
+
+
+
+
+
+
