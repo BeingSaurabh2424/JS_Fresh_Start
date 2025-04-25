4611670ee919bedef5acb667394f397dff30979b
BeingSaurabh2424 <saurabhwadekar24@gmail.com>
2025-04-25 18:27:14 +0530

    code

---
+++ 3. var_let_const/index.js
@@ -0,0 +1,21 @@
+console.log("var+let+const")
+
+var a = 45;
+var a = 'p'
+
+console.log(a)
+
+const author = "Saurabh"
+//author = "SanjuBaba"  // will throw an error assignment to constant variable
+
+console.log(author)
+b = 4  
+let c = null
+let d = undefined
+
+{
+    let b = 'this'
+    console.log(b)
+}
+console.log(b)
+
