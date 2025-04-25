e148048283f4f3e861ae71a99e580f626b82d591
BeingSaurabh2424 <saurabhwadekar24@gmail.com>
2025-04-25 18:38:57 +0530

    code

---
+++ 5. Practise Set/index.js
@@ -0,0 +1,31 @@
+//Question 1
+let a = "Sanjeevani"
+let b = 24
+console.log(a+b)
+
+//Question 2
+console.log(typeof (a+b))
+
+//Question 3
+const a1 = {
+    name : "Saurabh",
+    group : 4,
+    isMember : false 
+}
+
+//Question 4
+
+a1['friend'] = "Kalyani"
+a1['name'] = "Aditya"
+console.log(a1)
+
+// Question 5
+
+const dict = {
+    appreciate: "recognize the full worth of.",
+    ataraxia: "a state of freedom from emotional disturbance and anxiety",
+    yakka: "work, especially hard work."
+}
+
+console.log(dict['ataraxia'])
+
